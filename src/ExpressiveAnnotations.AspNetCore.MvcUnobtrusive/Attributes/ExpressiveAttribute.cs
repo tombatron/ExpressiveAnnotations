aba3f50106463b24fac9386ac39245ab095a4526
Tom Hanks <tommy.hanks@gmail.com>
2018-06-17 16:15:00 -0400

    Started implementation of the ExpressiveAttribute base class.

---
+++ src/ExpressiveAnnotations.AspNetCore.MvcUnobtrusive/Attributes/ExpressiveAttribute.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using ExpressiveAnnotations.Analysis;
+using ExpressiveAnnotations.Functions;
+using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace ExpressiveAnnotations.AspNetCore.MvcUnobtrusive.Attributes
+{
+    [AttributeUsage(AttributeTargets.Field | AttributeTargets.Property, AllowMultiple = true)]
+    public abstract class ExpressiveAttribute : ValidationAttribute, IClientModelValidator
+    {
+        public string Expression { get; }
+
+        protected Parser Parser { get; }
+        protected Dictionary<Type, Func<object, bool>> CachedValidationFuncs { get; }
+
+        protected ExpressiveAttribute(string expression, string errorMessage) : this(expression, () => errorMessage) { }
+
+        protected ExpressiveAttribute(string expression, Func<string> errorMessageAccessor) : base(errorMessageAccessor)
+        {
+            Expression = expression ?? throw new ArgumentException(nameof(expression), "Expression not provided.");
+            CachedValidationFuncs = new Dictionary<Type, Func<object, bool>>();
+
+            Parser = new Parser();
+            Parser.RegisterToolchain();
+        }
+
+        public void AddValidation(ClientModelValidationContext context)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
