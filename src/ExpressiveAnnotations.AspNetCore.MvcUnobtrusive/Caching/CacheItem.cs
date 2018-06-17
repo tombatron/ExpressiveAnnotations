ed8a10ba304213972702392bbde8111c58298d73
Tom Hanks <tommy.hanks@gmail.com>
2018-06-17 07:22:32 -0400

    Ported over the CacheItem class.
    This is apart of the ProcessStorage class. I just don't like putting
    multiple constructs in the same file.

---
+++ src/ExpressiveAnnotations.AspNetCore.MvcUnobtrusive/Caching/CacheItem.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace ExpressiveAnnotations.AspNetCore.MvcUnobtrusive.Caching
+{
+    internal class CacheItem
+    {
+        public IDictionary<string, string> FieldsMap { get; set; }
+        public IDictionary<string, object> ConstsMap { get; set; }
+        public IDictionary<string, object> EnumsMap { get; set; }
+        public IEnumerable<string> MethodsList { get; set; }
+        public IDictionary<string, string> ParsersMap { get; set; }
+    }
+}
