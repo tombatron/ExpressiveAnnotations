1c89a63cb2dd63df8507c3d1e740d5384a4369bc
Tom Hanks <tommy.hanks@gmail.com>
2018-06-17 07:29:47 -0400

    Ported over the rest of the ProcessStorage class.

--- src/ExpressiveAnnotations.AspNetCore.MvcUnobtrusive/Caching/ProcessStorage.cs
+++ src/ExpressiveAnnotations.AspNetCore.MvcUnobtrusive/Caching/ProcessStorage.cs
@@ -1,10 +1,20 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
+using System.Collections.Concurrent;
+using System.Threading;
 
 namespace ExpressiveAnnotations.AspNetCore.MvcUnobtrusive.Caching
 {
-    class ProcessStorage
+    internal static class ProcessStorage<TKey, TValue>
     {
+        private static readonly ConcurrentDictionary<TKey, Lazy<TValue>> _cache = new ConcurrentDictionary<TKey, Lazy<TValue>>();
+
+        public static TValue GetOrAdd(TKey key, Func<TKey, TValue> valueFactory)
+        {
+            var lazyResult = _cache.GetOrAdd(key, k => new Lazy<TValue>(() => valueFactory(k), LazyThreadSafetyMode.ExecutionAndPublication));
+
+            return lazyResult.Value;
+        }
+
+        public static void Clear() => _cache.Clear();
     }
 }
