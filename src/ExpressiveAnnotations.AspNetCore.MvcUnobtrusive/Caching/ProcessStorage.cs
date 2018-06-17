4c5441a5a0c066b202e4c72fd0dd009cfc96e5f4
Tom Hanks <tommy.hanks@gmail.com>
2018-06-17 07:16:38 -0400

    Ported RequestStorage to ASP.Net Core.
    Added the class (not implemented) for ProcessStorage.

---
+++ src/ExpressiveAnnotations.AspNetCore.MvcUnobtrusive/Caching/ProcessStorage.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExpressiveAnnotations.AspNetCore.MvcUnobtrusive.Caching
+{
+    class ProcessStorage
+    {
+    }
+}
---
+++ src/ExpressiveAnnotations.AspNetCore.MvcUnobtrusive/Caching/RequestStorage.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+
+namespace ExpressiveAnnotations.AspNetCore.MvcUnobtrusive.Caching
+{
+    internal class RequestStorage
+    {
+        private readonly HttpContext _httpContext;
+
+        internal RequestStorage(IHttpContextAccessor httpContextAccessor) => _httpContext = httpContextAccessor.HttpContext;
+
+        private IDictionary<object, object> Items
+        {
+            get
+            {
+                if (_httpContext == null)
+                {
+                    throw new ApplicationException("HttpContext not available.");
+                }
+
+                return _httpContext.Items;
+            }
+        }
+
+        internal T Get<T>(string key) => Items[key] == null ? default(T) : (T)Items[key];
+
+        internal void Set<T>(string key, T value) => Items[key] = value;
+
+        internal void Remove(string key) => Items.Remove(key);
+    }
+}
