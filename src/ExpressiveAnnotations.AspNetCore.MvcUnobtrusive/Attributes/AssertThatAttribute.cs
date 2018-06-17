2c2b3ae8a92c2498e7b0db589a088f12a9593fca
Tom Hanks <tommy.hanks@gmail.com>
2018-06-17 16:40:22 -0400

    Current progress getting the AssertThatAttribute ported.

---
+++ src/ExpressiveAnnotations.AspNetCore.MvcUnobtrusive/Attributes/AssertThatAttribute.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+
+namespace ExpressiveAnnotations.AspNetCore.MvcUnobtrusive.Attributes
+{
+    public sealed class AssertThatAttribute : ExpressiveAttribute
+    {
+        public AssertThatAttribute(string expression, string errorMessage) : base(expression, errorMessage) { }
+
+        public override void AddValidation(ClientModelValidationContext context)
+        {
+
+        }
+
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            if (value != null)
+            {
+                Compile(validationContext.ObjectType);
+
+                if (!CachedValidationFuncs[validationContext.ObjectType](validationContext.ObjectInstance))
+                {
+                    return new ValidationResult(
+                        FormatErrorMessage(validationContext.DisplayName, Expression, validationContext.ObjectInstance),
+                        new[] { validationContext.MemberName }
+                    );
+                }
+            }
+
+            return ValidationResult.Success;
+        }
+    }
+}
--- src/ExpressiveAnnotations.AspNetCore.MvcUnobtrusive/Attributes/ExpressiveAttribute.cs
+++ src/ExpressiveAnnotations.AspNetCore.MvcUnobtrusive/Attributes/ExpressiveAttribute.cs
@@ -1,9 +1,10 @@
-﻿using System.Collections.Generic;
-using ExpressiveAnnotations.Analysis;
+﻿using ExpressiveAnnotations.Analysis;
 using ExpressiveAnnotations.Functions;
 using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
 using System;
+using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.Linq;
 
 namespace ExpressiveAnnotations.AspNetCore.MvcUnobtrusive.Attributes
 {
@@ -26,9 +27,45 @@ namespace ExpressiveAnnotations.AspNetCore.MvcUnobtrusive.Attributes
             Parser.RegisterToolchain();
         }
 
-        public void AddValidation(ClientModelValidationContext context)
+        public virtual string FormatErrorMessage(string displayName, string expression, object objectInstance)
         {
-            throw new NotImplementedException();
+            try
+            {
+                IList<FormatItem> items;
+
+                var message = PreformatMessage(displayName, expression, out items);
+
+                message = items.Aggregate(message, (cargo, current) => current.Indicator != null && !current.Constant ? cargo.Replace(current.Uuid.ToString(), Helper.ExtractDisplayName(objectInstance.GetType(), current.FieldPath)) : cargo);
+                message = items.Aggregate(message, (cargo, current) => current.Indicator == null && !current.Constant ? cargo.Replace(current.Uuid.ToString(), (Helper.ExtractValue(objectInstance, current.FieldPath) ?? string.Empty).ToString()) : cargo);
+
+                return message;
+            }
+            catch (Exception ex)
+            {
+                throw new FormatException($"Problem with error message processing. The message is following: {ErrorMessageString}", ex);
+            }
+        }
+
+        public void Compile(Type validationContextType, Action<Parser> action = null)
+        {
+            if (!CachedValidationFuncs.ContainsKey(validationContextType))
+            {
+                CachedValidationFuncs[validationContextType] = Parser.Parse<bool>(validationContextType, Expression);
+            }
+
+            action?.Invoke(Parser);
+        }
+
+        public abstract void AddValidation(ClientModelValidationContext context);
+
+        private string PreformatMessage(string displayName, string expression, out IList<FormatItem> items)
+        {
+            var message = MessageFormatter.FormatString(ErrorMessageString, out items);
+
+            message = string.Format(message, displayName, expression);
+            message = items.Aggregate(message, (cargo, current) => cargo.Replace(current.Uuid.ToString(), current.Substitute));
+
+            return message;
         }
     }
 }
